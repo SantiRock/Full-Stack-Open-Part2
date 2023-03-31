2690f661292d813e625ac9c57f8ea8a0373052e2
Santiago Quintero <santiago.qp86@gmail.com>
2023-03-30 23:11:26 -0500

    2.3: Course information step8

--- src/courseinfo.js
+++ src/courseinfo.js
@@ -5,11 +5,20 @@ const Parts = ({ name, exercises}) => <p>{name}: {exercises}</p>
 const Content = ({ parts }) => parts.map(({name, exercises, id}) => 
 <Parts key={id} name={name} exercises={exercises}/> )
 
+const TotalExercices = ({ parts }) => {
+    const sum = (val, part) => val + part.exercises
+    const totalExercices = parts.reduce(sum, 0)
+    return (
+      <p><b>Total of {totalExercices} exercises</b></p>
+    )
+}
+
 const Course = ({ course }) => {
     return (
      <>
         <Header name={course.name}/>
         <Content parts={course.parts}/>
+        <TotalExercices parts={course.parts} />
     </>
     )
 }
@@ -33,6 +42,11 @@ const Courseinfo = () => {
             name: 'State of a component',
             exercises: 14,
             id: 3
+          },
+          {
+            name: 'Redux',
+            exercises: 11,
+            id: 4
           }
         ]
       }
