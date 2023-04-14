40cc10191ce57bcc357b7a82f5ca462db675156b
Santiago Quintero <santiago.qp86@gmail.com>
2023-04-13 20:32:59 -0500

    2.10: The Phonebook Step5

--- src/pages/phonebook.js
+++ src/pages/phonebook.js
@@ -1,16 +1,61 @@
 import { useState } from 'react'
 
+const Filter = ( {handleFilter} ) => {
+    return (
+        <form>
+            <div>
+                Filter shown with <input
+                onChange={handleFilter}
+                />
+            </div>
+        </form>
+    )
+}
+
+const PersonForm = ( {addPerson, newName, handlePersonChange, 
+    newNumber, handleNumberChange} ) => {
+        return (
+            <form onSubmit={addPerson}>
+                <div>
+                    Name: <input
+                    value={newName}
+                    onChange={handlePersonChange} 
+                    />
+                </div>
+                <p></p>
+                <div>
+                    Number: <input
+                    value={newNumber}
+                    onChange={handleNumberChange}
+                    />
+                </div>
+                <p></p>
+                <div>
+                    <button type='submit' >add</button>
+                </div>
+            </form>
+        )
+    }
+
+const Person = ( { name, number} ) => <p>{name} - {number}</p>
+
+const Persons = ( {showAll} ) => showAll.map(person => 
+<Person key={person.id} name={person.name} number={person.number}/>) 
 
 const Phonebook = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-1234567'}
+        { name: 'Arto Hellas', number: '040-123456', id: 1 },
+        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
     ])
 
-    const [newName, setName] = useState('');
-    const [newNumber, setNumber] =useState('');
+    const [newName, setName] = useState('')
+    const [newNumber, setNumber] =useState('')
+    const [showAll, setShowAll] = useState(persons)
 
     const addPerson = (event) => {
-    if (persons.some(person => person.name === newName)) {
+    if (persons.some(person => person.name.toLowerCase() === newName.toLowerCase())) {
         alert(newName + ' is already added you phonebook')
         setName('')
         setNumber('')
@@ -18,9 +63,11 @@ const Phonebook = () => {
         event.preventDefault()
         const personObject = {
             name: newName,
-            number: newNumber
+            number: newNumber,
+            id: persons.length + 1
         }
         setPersons(persons.concat(personObject))
+        setShowAll(persons.concat(personObject))
         setName('')
         setNumber('')
         }
@@ -34,29 +81,21 @@ const Phonebook = () => {
     setNumber(event.target.value)
    }
 
+   const handleFilter = (event) => {
+    let word = event.target.value.toLowerCase()
+    setShowAll(persons.filter(person => person.name.toLowerCase().includes(word)))
+   }
+
     return(
         <>
             <h2>Phonebook</h2>
-            <form onSubmit={addPerson}>
-                <div>
-                    name: <input
-                    value={newName}
-                    onChange={handlePersonChange} 
-                    />
-                </div>
-                <div>
-                    number: <input
-                    value={newNumber}
-                    onChange={handleNumberChange}
-                    />
-                </div>
-                <div>
-                    <button type='submit' >add</button>
-                </div>
-            </form>
+            <Filter handleFilter={handleFilter}/>
+            <h2>Add a new contact</h2>
+            <PersonForm addPerson={addPerson} newName={newName}
+            handlePersonChange={handlePersonChange} newNumber={newNumber}
+            handleNumberChange={handleNumberChange}/>
             <h2>Numbers</h2>
-            {persons.map(person => 
-                <p key={person.name}>{person.name} {person.number}</p>)}
+            <Persons showAll={showAll} />
         </>
     )
 }
