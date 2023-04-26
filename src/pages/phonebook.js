f8ac5bdb8a5156dca7914eefde0e7978d354b863
Santiago Quintero <santiago.qp86@gmail.com>
2023-04-25 22:10:13 -0500

    2.11: The Phonebook Step 6

--- src/pages/phonebook.js
+++ src/pages/phonebook.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ( {handleFilter} ) => {
     return (
@@ -42,17 +43,30 @@ const Person = ( { name, number} ) => <p>{name} - {number}</p>
 const Persons = ( {showAll} ) => showAll.map(person => 
 <Person key={person.id} name={person.name} number={person.number}/>) 
 
-const Phonebook = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ])
+const Section = ( {title, children} ) => {
+    return(
+        <section>
+            <h2>{title}</h2>
+            {children}
+        </section>
+    )
+}
 
+
+const Phonebook = () => {
+    const [persons, setPersons] = useState([])
     const [newName, setName] = useState('')
     const [newNumber, setNumber] =useState('')
     const [showAll, setShowAll] = useState(persons)
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                setPersons(response.data)
+                setShowAll(response.data)
+            })
+    }, [])  
 
     const addPerson = (event) => {
     if (persons.some(person => person.name.toLowerCase() === newName.toLowerCase())) {
@@ -88,14 +102,17 @@ const Phonebook = () => {
 
     return(
         <>
-            <h2>Phonebook</h2>
-            <Filter handleFilter={handleFilter}/>
-            <h2>Add a new contact</h2>
-            <PersonForm addPerson={addPerson} newName={newName}
-            handlePersonChange={handlePersonChange} newNumber={newNumber}
-            handleNumberChange={handleNumberChange}/>
-            <h2>Numbers</h2>
-            <Persons showAll={showAll} />
+            <Section title={'Phonebook'}>
+                <Filter handleFilter={handleFilter}/>
+            </Section>
+            <Section title={'Add a new contact'} >
+                <PersonForm addPerson={addPerson} newName={newName}
+                handlePersonChange={handlePersonChange} newNumber={newNumber}
+                handleNumberChange={handleNumberChange}/>
+            </Section>
+            <Section title={'Numbers'}>
+               <Persons showAll={showAll} />
+            </Section>
         </>
     )
 }
